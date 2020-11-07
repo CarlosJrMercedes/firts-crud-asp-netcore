c8e49c6a949600f089d170ba860bc5c76f785807
CarlosJr <carlooosmercedes@gmail.com>
2020-11-07 02:49:39 -0600

    firts-crud

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProductWeb.Models;
+
+namespace ProductWeb.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Position> Positions { get; set; }
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWeb.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirtsName { get; set; }
+        public string LastName { get; set; }
+        public int PositionId { get; set; }
+        public Position position{ get; set; }
+    }
+}
